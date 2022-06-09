f98d9bca3f53199a416223ee4c77dd1e5c78f0e5
Vinh Duong <xvinh.ir@gmail.com>
2022-06-09 16:17:41 +0700

    solution

--- controllers/car.controller.js
+++ controllers/car.controller.js
@@ -4,33 +4,55 @@ const carController = {};
 
 carController.createCar = async (req, res, next) => {
 	try {
-		// YOUR CODE HERE
+		const { make, model, release_date, transmission_type, size, style, price } = req.body;
+		if (!make || !model || !release_date || !transmission_type || !size || !style || !price) {
+			throw new Error('Missing required info!');
+		}
+		const car = await Car.create({ make, model, release_date, transmission_type, size, style, price });
+
+		return res.status(200).send({ message: 'Create Car Successfully!', car });
 	} catch (err) {
-		// YOUR CODE HERE
+		res.status(400).send({ message: err.message });
 	}
 };
 
 carController.getCars = async (req, res, next) => {
 	try {
-		// YOUR CODE HERE
+		const page = parseInt(req.query.page) || 1;
+		const limit = req.query.limit || 10;
+		const cars = await Car.find()
+			.sort({ createdAt: -1 })
+			.skip((page - 1) * limit)
+			.limit(limit);
+		const total = await Car.countDocuments({ isDeleted: false });
+		return res.status(200).json({ cars, page, total: Math.ceil(total / limit) });
 	} catch (err) {
-		// YOUR CODE HERE
+		res.status(400).send({ message: err.message });
 	}
 };
 
 carController.editCar = async (req, res, next) => {
 	try {
-		// YOUR CODE HERE
+		const { id } = req.params;
+		if (!mongoose.isValidObjectId(id)) throw new Error('Invalid ID');
+
+		const car = await Car.findByIdAndUpdate(id, { ...req.body }, { new: true, runValidators: true });
+		if (!car) throw new Error('Car not found!');
+		return res.status(200).send({ message: 'Update Car Successfully!', car });
 	} catch (err) {
-		// YOUR CODE HERE
+		res.status(400).send({ message: err.message });
 	}
 };
 
 carController.deleteCar = async (req, res, next) => {
 	try {
-		// YOUR CODE HERE
+		const { id } = req.params;
+		if (!mongoose.isValidObjectId(id)) throw new Error('Invalid ID');
+		const car = await Car.findByIdAndUpdate(id, { isDeleted: true }, { new: true, runValidators: true });
+		if (!car) throw new Error('Car not found!');
+		return res.status(200).send({ message: 'Delete Car Successfully!', car });
 	} catch (err) {
-		// YOUR CODE HERE
+		res.status(400).send({ message: err.message });
 	}
 };
 
